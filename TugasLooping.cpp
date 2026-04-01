8acb5b547935f1b3b00aa7c30af225cdb1e5481b
penyelamatbumi <inihanip7@gmail.com>
2026-04-01 16:04:45 +0800

    menambahkan variabel

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -108,5 +108,20 @@ void showMenu()
     cout << "Pilih menu: ";
 }
 
+#include <iostream>
+
+using namespace std;
+
+// variabel global
+int n;
+int pilihan;
+
+// --- Prototipe Fungsi ---
+bool isPrime(int num);
+bool isFibonacci(int num);
+void inputNumber();
+void displayResult(int type);
+void showMenu();
+
 
 
