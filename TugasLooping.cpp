259b6fd183549daab7261a7b757ee0d3cc37465a
penyelamatbumi <inihanip7@gmail.com>
2026-04-01 16:00:52 +0800

    first commit

---
+++ TugasLooping.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// variabel global
+int n;
+int pilihan;
+
+// --- Prototipe Fungsi ---
+bool isPrime(int num);
+bool isFibonacci(int num);
+void inputNumber();
+void displayResult(int type);
+void showMenu();
+
